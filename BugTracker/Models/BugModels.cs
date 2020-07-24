366d006b2998c53fc0117e92c1024b230ba7c978
Luca <lucapeddy001@gmail.com>
2020-07-24 11:35:41 +0200

    Add project files.

---
+++ BugTracker/Models/BugModels.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Models
+{
+    public class BugModels
+    {
+        [JsonProperty("autore")]
+        public string Autore { get; set; }
+
+        [JsonProperty("data")]
+        public DateTimeOffset Data { get; set; } = DateTime.Parse(DateTime.Now.Date.ToShortDateString());
+
+        [JsonProperty("descrizione")]
+        public string Descrizione { get; set; }
+
+        [JsonProperty("stato")]
+        public bool Stato { get; set; }
+
+        [JsonProperty("titolo")]
+        public string Titolo { get; set; }
+
+    }
+}
